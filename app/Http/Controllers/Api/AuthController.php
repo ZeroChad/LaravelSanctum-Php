56e5667d017dec8957a3f47bf9a8b35e5e5741c0
ZeroChad <drakeescarda13@gmail.com>
2025-11-03 16:03:30 +0800

    error test

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
     
     public function register(Request $request)
     {
-        validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:8',
             'first_name' => 'required|string|max:255',
@@ -27,5 +27,10 @@ class AuthController extends Controller
             'sex' => 'required|in:male,female,other',
             'birthday' => 'required|date|before:today',
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()], 422);
+        }
     }
 }
