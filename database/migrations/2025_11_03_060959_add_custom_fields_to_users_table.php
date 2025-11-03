45964e754527bf60f6077636c5d35f6d103816dd
ZeroChad <drakeescarda13@gmail.com>
2025-11-03 14:54:08 +0800

    forgot to commit mb: Laravel JWT authentication api

---
+++ database/migrations/2025_11_03_060959_add_custom_fields_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->after('name');
+            $table->string('last_name')->after('first_name');
+            $table->enum('role', ['admin','user','Stocker','Client'])->default('user')->after('email');
+            $table->enum('sex', ['male','female','other'])->default('male')->after('role');
+            $table->date('birthday')->after('sex');
+            //
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'role', 'sex', 'birthday']);
+        });
+    }
+};
